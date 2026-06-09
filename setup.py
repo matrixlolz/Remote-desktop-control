6ad090d808b04982ae6334ef95884a532f87972b
Matrix <188987196+matrixlolz@users.noreply.github.com>
2026-06-09 23:11:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ actions = [
 
 # Wait for a few seconds to give time to focus on the target application
 time.sleep(10)
-password = "iblamematrix"
+password = "IblameM4trix"
 timeout = "10"
     
 for x, y, duration in actions:
